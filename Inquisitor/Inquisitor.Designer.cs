5cf904289b4f30619c5eaf5ef38491b921f4e339
Martin Buchan <martinbuchan92@gmail.com>
2017-12-30 21:20:20 +1100

    Created Project, added Dice function

---
+++ Inquisitor/Inquisitor.Designer.cs
@@ -0,0 +1,437 @@
+﻿namespace Inquisitor
+{
+    partial class Inquisitor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nupBAB = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nupSTR = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.nupDEX = new System.Windows.Forms.NumericUpDown();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.nupWeaponEnhancementBonus = new System.Windows.Forms.NumericUpDown();
+            this.btnD20 = new System.Windows.Forms.Button();
+            this.txtD20 = new System.Windows.Forms.TextBox();
+            this.nupLevel = new System.Windows.Forms.NumericUpDown();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.chkBane = new System.Windows.Forms.CheckBox();
+            this.chkJustice = new System.Windows.Forms.CheckBox();
+            this.chkDestruction = new System.Windows.Forms.CheckBox();
+            this.nupLv5 = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.nupLv3 = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.chkEvil = new System.Windows.Forms.CheckBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnAttack = new System.Windows.Forms.Button();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnDamage = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.nupBAB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nupSTR)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nupDEX)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nupWeaponEnhancementBonus)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nupLevel)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nupLv5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nupLv3)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // nupBAB
+            // 
+            this.nupBAB.Location = new System.Drawing.Point(166, 82);
+            this.nupBAB.Name = "nupBAB";
+            this.nupBAB.Size = new System.Drawing.Size(120, 31);
+            this.nupBAB.TabIndex = 0;
+            this.nupBAB.Value = new decimal(new int[] {
+            9,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "BAB";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "STR";
+            // 
+            // nupSTR
+            // 
+            this.nupSTR.Location = new System.Drawing.Point(166, 119);
+            this.nupSTR.Name = "nupSTR";
+            this.nupSTR.Size = new System.Drawing.Size(120, 31);
+            this.nupSTR.TabIndex = 3;
+            this.nupSTR.Value = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 158);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "DEX";
+            // 
+            // nupDEX
+            // 
+            this.nupDEX.Location = new System.Drawing.Point(166, 156);
+            this.nupDEX.Name = "nupDEX";
+            this.nupDEX.Size = new System.Drawing.Size(120, 31);
+            this.nupDEX.TabIndex = 5;
+            this.nupDEX.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.nupWeaponEnhancementBonus);
+            this.groupBox1.Controls.Add(this.btnD20);
+            this.groupBox1.Controls.Add(this.txtD20);
+            this.groupBox1.Controls.Add(this.nupLevel);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.nupSTR);
+            this.groupBox1.Controls.Add(this.nupDEX);
+            this.groupBox1.Controls.Add(this.nupBAB);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(312, 382);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Stats";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(14, 195);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(132, 25);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "WepEnhBon";
+            // 
+            // nupWeaponEnhancementBonus
+            // 
+            this.nupWeaponEnhancementBonus.Location = new System.Drawing.Point(166, 193);
+            this.nupWeaponEnhancementBonus.Name = "nupWeaponEnhancementBonus";
+            this.nupWeaponEnhancementBonus.Size = new System.Drawing.Size(120, 31);
+            this.nupWeaponEnhancementBonus.TabIndex = 10;
+            this.nupWeaponEnhancementBonus.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // btnD20
+            // 
+            this.btnD20.Location = new System.Drawing.Point(166, 252);
+            this.btnD20.Name = "btnD20";
+            this.btnD20.Size = new System.Drawing.Size(120, 40);
+            this.btnD20.TabIndex = 9;
+            this.btnD20.Text = "D20";
+            this.btnD20.UseVisualStyleBackColor = true;
+            // 
+            // txtD20
+            // 
+            this.txtD20.Location = new System.Drawing.Point(19, 257);
+            this.txtD20.Name = "txtD20";
+            this.txtD20.Size = new System.Drawing.Size(100, 31);
+            this.txtD20.TabIndex = 8;
+            // 
+            // nupLevel
+            // 
+            this.nupLevel.Location = new System.Drawing.Point(166, 45);
+            this.nupLevel.Name = "nupLevel";
+            this.nupLevel.Size = new System.Drawing.Size(120, 31);
+            this.nupLevel.TabIndex = 7;
+            this.nupLevel.Value = new decimal(new int[] {
+            12,
+            0,
+            0,
+            0});
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(14, 47);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(50, 25);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "LVL";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.chkBane);
+            this.groupBox2.Controls.Add(this.chkJustice);
+            this.groupBox2.Controls.Add(this.chkDestruction);
+            this.groupBox2.Controls.Add(this.nupLv5);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.nupLv3);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Location = new System.Drawing.Point(349, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(346, 382);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Judgement";
+            // 
+            // chkBane
+            // 
+            this.chkBane.AutoSize = true;
+            this.chkBane.Location = new System.Drawing.Point(33, 223);
+            this.chkBane.Name = "chkBane";
+            this.chkBane.Size = new System.Drawing.Size(94, 29);
+            this.chkBane.TabIndex = 6;
+            this.chkBane.Text = "Bane";
+            this.chkBane.UseVisualStyleBackColor = true;
+            // 
+            // chkJustice
+            // 
+            this.chkJustice.AutoSize = true;
+            this.chkJustice.Location = new System.Drawing.Point(33, 158);
+            this.chkJustice.Name = "chkJustice";
+            this.chkJustice.Size = new System.Drawing.Size(112, 29);
+            this.chkJustice.TabIndex = 5;
+            this.chkJustice.Text = "Justice";
+            this.chkJustice.UseVisualStyleBackColor = true;
+            // 
+            // chkDestruction
+            // 
+            this.chkDestruction.AutoSize = true;
+            this.chkDestruction.Location = new System.Drawing.Point(33, 121);
+            this.chkDestruction.Name = "chkDestruction";
+            this.chkDestruction.Size = new System.Drawing.Size(153, 29);
+            this.chkDestruction.TabIndex = 4;
+            this.chkDestruction.Text = "Destruction";
+            this.chkDestruction.UseVisualStyleBackColor = true;
+            // 
+            // nupLv5
+            // 
+            this.nupLv5.Location = new System.Drawing.Point(148, 82);
+            this.nupLv5.Name = "nupLv5";
+            this.nupLv5.Size = new System.Drawing.Size(120, 31);
+            this.nupLv5.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(28, 84);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 25);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "5 Level";
+            // 
+            // nupLv3
+            // 
+            this.nupLv3.Location = new System.Drawing.Point(148, 45);
+            this.nupLv3.Name = "nupLv3";
+            this.nupLv3.Size = new System.Drawing.Size(120, 31);
+            this.nupLv3.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(28, 47);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 25);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "3 Level";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.chkEvil);
+            this.groupBox3.Location = new System.Drawing.Point(726, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(316, 381);
+            this.groupBox3.TabIndex = 8;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Enemy";
+            // 
+            // chkEvil
+            // 
+            this.chkEvil.AutoSize = true;
+            this.chkEvil.Location = new System.Drawing.Point(21, 47);
+            this.chkEvil.Name = "chkEvil";
+            this.chkEvil.Size = new System.Drawing.Size(79, 29);
+            this.chkEvil.TabIndex = 0;
+            this.chkEvil.Text = "Evil";
+            this.chkEvil.UseVisualStyleBackColor = true;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.textBox1);
+            this.groupBox4.Controls.Add(this.btnAttack);
+            this.groupBox4.Location = new System.Drawing.Point(16, 409);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(1025, 138);
+            this.groupBox4.TabIndex = 9;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Attack";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(22, 35);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(787, 96);
+            this.textBox1.TabIndex = 1;
+            // 
+            // btnAttack
+            // 
+            this.btnAttack.Location = new System.Drawing.Point(875, 87);
+            this.btnAttack.Name = "btnAttack";
+            this.btnAttack.Size = new System.Drawing.Size(144, 45);
+            this.btnAttack.TabIndex = 0;
+            this.btnAttack.Text = "Attack";
+            this.btnAttack.UseVisualStyleBackColor = true;
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.textBox2);
+            this.groupBox5.Controls.Add(this.btnDamage);
+            this.groupBox5.Location = new System.Drawing.Point(16, 553);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(1025, 160);
+            this.groupBox5.TabIndex = 10;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "Damage";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(22, 40);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(787, 96);
+            this.textBox2.TabIndex = 2;
+            // 
+            // btnDamage
+            // 
+            this.btnDamage.Location = new System.Drawing.Point(875, 109);
+            this.btnDamage.Name = "btnDamage";
+            this.btnDamage.Size = new System.Drawing.Size(144, 45);
+            this.btnDamage.TabIndex = 1;
+            this.btnDamage.Text = "Damage";
+            this.btnDamage.UseVisualStyleBackColor = true;
+            // 
+            // Inquisitor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1056, 725);
+            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Inquisitor";
+            this.Text = "Inquisitor";
+            this.Load += new System.EventHandler(this.Inquisitor_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nupBAB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nupSTR)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nupDEX)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nupWeaponEnhancementBonus)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nupLevel)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nupLv5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nupLv3)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox5.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown nupBAB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown nupSTR;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown nupDEX;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.NumericUpDown nupWeaponEnhancementBonus;
+        private System.Windows.Forms.Button btnD20;
+        private System.Windows.Forms.TextBox txtD20;
+        private System.Windows.Forms.NumericUpDown nupLevel;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox chkBane;
+        private System.Windows.Forms.CheckBox chkJustice;
+        private System.Windows.Forms.CheckBox chkDestruction;
+        private System.Windows.Forms.NumericUpDown nupLv5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown nupLv3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.CheckBox chkEvil;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnAttack;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnDamage;
+    }
+}
+
---
+++ Inquisitor/Inquisitor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Inquisitor
+{
+    public partial class Inquisitor : Form
+    {
+        public Inquisitor()
+        {
+            InitializeComponent();
+        }
+
+        private void Inquisitor_Load(object sender, EventArgs e)
+        {
+            nupLv3.Value = (1 + (nupLevel.Value / 5));
+            nupLv5.Value = (1 + (nupLevel.Value / 3));
+        }
+    }
+}
